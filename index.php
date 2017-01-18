32621c8da0fb85c44ea3b90b44d745490cac7970
griffindotson <griffin_dotson@yahoo.com>
2017-01-17 18:59:00 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 echo '
-    
+    <title>Temperature Converter</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
     <form method = "post" action = "index.php">
     <h1>Temperature Converter</h1>
-    Enter a temperature <input type ="number" name="TempDegree" /><br />
+    Enter a temperature <input type ="number" step="0.01" name="TempDegree" /><br />
+    <br>
     <select name="Temp1">
         <option value="f">Fahrenheit</option>
         <option value="c">Celsius</option>
@@ -16,43 +18,59 @@ echo '
         <option value="k">Kelvin</option>
     </select>
     <br>
+    <br>
     <input type ="submit" value="Convert" /><br />
-    </form>
-    ';//User Interface
+    <div>
+    ';
 
-if($_POST){//show conversion
+if($_POST){//show data
     $temp1 = $_POST['Temp1'];
     $temp2 = $_POST['Temp2'];
     $tempIn = $_POST['TempDegree'];
    
-    //Error message if no input 
     if($tempIn == NULL){
         echo 'Enter a temperature';
     }
     else if($temp1 == $temp2){
-        echo $tempIn;
+        if($temp1 == 'f')
+        {
+            echo $tempIn.'&#8457';
+        }else if($temp1 == 'c')
+        {
+            echo $tempIn.'&#8451';    
+        }else if($temp1 == 'k')
+        {
+            echo $tempIn.'&#8490';    
+        }
+        
         
     }else if($temp1 == 'f' && $temp2 == 'c'){
-        echo 5/9*($tempIn-32);
+        echo round(5/9*($tempIn-32),2).'&#8451';
         
     }else if($temp1 == 'f' && $temp2 == 'k'){
-        echo 5/9*($tempIn-32)+273;
+        echo round(5/9*($tempIn-32)+273,2).'&#8490';
         
     }else if($temp1 == 'c' && $temp2 == 'f'){
-        echo 9/5*($tempIn)+32;
+        echo round(9/5*($tempIn)+32,2).'&#8457';
         
     }else if($temp1 == 'c' && $temp2 == 'k'){
-        echo $tempIn+273;
+        echo round($tempIn+273,2).'&#8490';
         
     }else if($temp1 == 'k' && $temp2 == 'f'){
-        echo 9/5*($tempIn-273)+32;
+        echo round(9/5*($tempIn-273)+32,2).'&#8457';
         
     }else if($temp1 == 'k' && $temp2 == 'c'){
-        echo $tempIn-273;
+        echo round($tempIn-273,2).'&#8451';
         
     }
     
 }
+
+echo 
+    '
+    </div>
+    </form>
+    ';
 
 
 
